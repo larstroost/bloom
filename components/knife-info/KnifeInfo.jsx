0aec0ebf7518f9fdd01ba9e5cc77568d954cfd0a
Lars Troost <42066432+larstroost@users.noreply.github.com>
2018-10-10 14:27:48 +0200

    Start of KnifeInfo component

---
+++ components/knife-info/KnifeInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './knife-info.scss';
+
+class KnifeInfo extends React.Component {
+  //state en functies
+  render() {
+    //destructuring and props
+    return (
+      //HTML
+
+    )
+  }
+}
