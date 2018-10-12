25151a3ec192a3516cd39fb8315217474f2710da
Lars Troost <42066432+larstroost@users.noreply.github.com>
2018-10-12 13:11:18 +0200

    KnifeInfo in progress

--- components/knife-info/KnifeInfo.jsx
+++ components/knife-info/KnifeInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import './knife-info.scss';
 
@@ -15,7 +16,7 @@ class KnifeInfo extends React.Component {
   render() {
     //destructuring and props
     const { isOpen } = this.state;
-    const { title, src } = this.props;
+    const { title, src, basicInfo, extendedInfo } = this.props;
     return (
       //HTML
       <section
@@ -30,34 +31,52 @@ class KnifeInfo extends React.Component {
           src={src}
           alt="Kitchen Knife"
         />
-        <div className="knife-info__wrapper">
-          <h2 className="knife-info__title">
-            {title}
-          </h2>
-          {!this.state.isOpen && (
-            <p className="knife-info__text">
-              Placeholder basic info
-            </p>
-          )}
-          {this.state.isOpen && (
-            <p className="knife-info__text">
-              Placeholder tering veel tekst
-            </p>
-          )}
-          {!isOpen && (
-            <img
-              className="knife-info__arrow"
-              src="static/icons/arrow-down-2.svg"
-              alt="Arrow Down"
-            />
-          )}
-          {isOpen && (
-            <img
-              className="knife-info__arrow"
-              src="static/icons/arrow-up.svg"
-              alt="Arrow Up"
-            />
-          )}
+        <div className="knife-info__wrapper-rechts">
+          <div className="knife-info__wrapper">
+            <h2 className="knife-info__title">
+              {title}
+            </h2>
+            {!this.state.isOpen && (
+              <p className="knife-info__text">
+                {basicInfo}
+              </p>
+            )}
+            {this.state.isOpen && (
+              <p className="knife-info__text">
+                {extendedInfo}
+              </p>
+            )}
+            {!isOpen && (
+              <img
+                className="knife-info__arrow"
+                src="static/icons/arrow-down-2.svg"
+                alt="Arrow Down"
+              />
+            )}
+            {isOpen && (
+              <img
+                className="knife-info__arrow"
+                src="static/icons/arrow-up.svg"
+                alt="Arrow Up"
+              />
+            )}
+          </div>
+          <div className="knife-info__wrapper-linkage">
+            <Link href="/offerte">
+              <button className="knife-info__button knife-info__button--left">
+                <a className="knife-info__link">
+                  Offerte Aanvragen
+                </a>
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="knife-info__button">
+                <a className="knife-info__link">
+                  Meer Informatie Opvragen
+                </a>
+              </button>
+            </Link>
+          </div>
         </div>
       </section>
     );
--- pages/messen.js
+++ pages/messen.js
@@ -6,8 +6,18 @@ import KnifeInfo from '../components/knife-info/KnifeInfo';
 const Knives = () => (
   <React.Fragment>
     <MainNav page='messen' />
-    <KnifeInfo title="Keukenmessen" src="/static/icons/kitchen-knife.jpg" />
-    <KnifeInfo title="Koksmessen" src="/static/icons/chef-knife.jpeg" />
+    <KnifeInfo
+      title="Keukenmessen"
+      src="/static/icons/kitchen-knife.jpg"
+      basicInfo="Basis informatie over keukenmessen"
+      extendedInfo="Uitgebreide informatie over keukenmessen"
+    />
+    <KnifeInfo
+      title="Koksmessen"
+      src="/static/icons/chef-knife.jpeg"
+      basicInfo="Basis informatie over koksmessen"
+      extendedInfo="Uitgebreide informatie over koksmessen"
+    />
   </React.Fragment>
 );
 
