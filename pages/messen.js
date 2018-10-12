62cda1bab5557d5e4dcbff46bb3114a50bb3e61f
Lars Troost <42066432+larstroost@users.noreply.github.com>
2018-10-12 15:02:09 +0200

    KnifeInfo in progress, adding some scaling

--- pages/messen.js
+++ pages/messen.js
@@ -6,18 +6,18 @@ import KnifeInfo from '../components/knife-info/KnifeInfo';
 const Knives = () => (
   <React.Fragment>
     <MainNav page='messen' />
-    <KnifeInfo
-      title="Keukenmessen"
-      src="/static/icons/kitchen-knife.jpg"
-      basicInfo="Basis informatie over keukenmessen"
-      extendedInfo="Uitgebreide informatie over keukenmessen"
-    />
-    <KnifeInfo
-      title="Koksmessen"
-      src="/static/icons/chef-knife.jpeg"
-      basicInfo="Basis informatie over koksmessen"
-      extendedInfo="Uitgebreide informatie over koksmessen"
-    />
+      <KnifeInfo
+        title="Keukenmessen"
+        src="/static/icons/kitchen-knife.jpg"
+        basicInfo="Basis informatie over keukenmessen"
+        extendedInfo="Uitgebreide informatie over keukenmessen"
+      />
+      <KnifeInfo
+        title="Koksmessen"
+        src="/static/icons/chef-knife.jpeg"
+        basicInfo="Basis informatie over koksmessen"
+        extendedInfo="Uitgebreide informatie over koksmessen"
+      />
   </React.Fragment>
 );
 
