03e9bd9e766db9e8dc9443ebfbc1a0f1258ad123
Lars Troost <42066432+larstroost@users.noreply.github.com>
2018-10-22 17:58:57 +0200

    Knife Info in progress

--- components/knife-info-plus/KnifeInfoPlus.jsx
+++ components/knife-info-plus/KnifeInfoPlus.jsx
@@ -68,11 +68,9 @@ class KnifeInfoPlus extends React.Component {
             knife-info-plus__wrapper-right--${isOpen ? 'open' : 'closed'}
           `}
         >
-          <div className="knife-info-plus__opacity-info">
-            <p className="knife-info-plus__info">
-              Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.
-            </p>
-          </div>
+          <p className="knife-info-plus__info">
+            Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.
+          </p>
           <div className="knife-info-plus__link-wrapper">
             <Link href="/offerte">
               <button className="knife-info-plus__button knife-info-plus__button--left">
