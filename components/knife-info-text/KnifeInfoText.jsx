797ce63a8c8d1ab7c948017b7d8f6b5f2269b225
MrNexz <43969336+MrNexz@users.noreply.github.com>
2018-10-29 23:50:11 +0100

    scharen pagina gevuld en props gemaakt

--- components/knife-info-text/KnifeInfoText.jsx
+++ components/knife-info-text/KnifeInfoText.jsx
@@ -4,26 +4,28 @@ import PropTypes from 'prop-types';
 
 import './knife-info-text.scss';
 
-const KnifeInfoText = ({infoTitle}) => (
+const KnifeInfoText = ({infoTitle, infoText, infoImg}) => (
   <div className="info-box">
-   <div className="info-box__wrapper">
-    <h1 className="info-box__title">
-     {infoTitle}
-    </h1>
-    <p className="info-box__text">
-     Heel veel text over verschillende soorten messen hier komt veel informatie over messen nog meer informatie over wat er op deze pagina komt <br/>nog meer, want ik vind br in de code leuk <br/>en lars niet
-    </p>
-   </div>
-   <img
-     className="info-box__image"
-     src="static/icons/kitchen-knife.jpg"
-     alt="Kitchen Knife"
-   />
+    <div className="info-box__wrapper">
+      <h1 className="info-box__title">
+      {infoTitle}
+      </h1>
+      <p className="info-box__text">
+      {infoText}
+      </p>
+    </div>
+    <img
+      className="info-box__image"
+      src={infoImg}
+      alt="Kitchen Knife"
+    />
   </div>
 );
 
 KnifeInfoText.propTypes = {
-  infoTitle: PropTypes.string.isRequired
+  infoTitle: PropTypes.string.isRequired,
+  infoText: PropTypes.string.isRequired,
+  infoImg: PropTypes.string.isRequired
 };
 
 export default KnifeInfoText;
--- pages/scharen.js
+++ pages/scharen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import MainNav from '../components/header/Header';
+import KnifeInfoText from '../components/knife-info-text/KnifeInfoText';
 
 const Sciccors = () => (
   <React.Fragment>
     <MainNav page='scharen' />
+    <KnifeInfoText
+    infoTitle="Scharen Slijpen"
+    infoText="Deze pagina moet veel tekst gaan bevatten over het slijpen van scharen. Buiten messen slijpt Horeca Slijperij Bloom ook scharen. Grote scharen, kleine scharen, botte scharen, minder scherpe scharen, scharen voor vlees, scharen voor takken, bomen, bladeren, groenten, plastic etc. Lever uw schaar in en wij slijpen het knipvlak weer scherp."
+    infoImg="../../static/icons/kitchen-knife.jpg"
+    />
     <p>
       COMING SOON
     </p>
