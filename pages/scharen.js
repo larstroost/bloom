c6a0114973ad3059b29c69e3165384848e131460
Lars Troost <42066432+larstroost@users.noreply.github.com>
2018-10-31 12:55:00 +0100

    Merge conflict

--- pages/scharen.js
+++ pages/scharen.js
@@ -1,31 +1,22 @@
 import React from 'react';
 
 import MainNav from '../components/header/Header';
-<<<<<<< HEAD
 import Footer from '../components/footer/Footer';
-=======
 import KnifeInfoText from '../components/knife-info-text/KnifeInfoText';
->>>>>>> master
 
 const Sciccors = () => (
   <React.Fragment>
     <MainNav page='scharen' />
-<<<<<<< HEAD
+    <KnifeInfoText
+      infoTitle="Scharen Slijpen"
+      infoText="Deze pagina moet veel tekst gaan bevatten over het slijpen van scharen. Buiten messen slijpt Horeca Slijperij Bloom ook scharen. Grote scharen, kleine scharen, botte scharen, minder scherpe scharen, scharen voor vlees, scharen voor takken, bomen, bladeren, groenten, plastic etc. Lever uw schaar in en wij slijpen het knipvlak weer scherp."
+      src="../../static/icons/kitchen-knife.jpg"
+      alt="Scissors"
+    />
     <p>
       COMING SOON
     </p>
     <Footer />
-=======
-      <KnifeInfoText
-        infoTitle="Scharen Slijpen"
-        infoText="Deze pagina moet veel tekst gaan bevatten over het slijpen van scharen. Buiten messen slijpt Horeca Slijperij Bloom ook scharen. Grote scharen, kleine scharen, botte scharen, minder scherpe scharen, scharen voor vlees, scharen voor takken, bomen, bladeren, groenten, plastic etc. Lever uw schaar in en wij slijpen het knipvlak weer scherp."
-        src="../../static/icons/kitchen-knife.jpg"
-        alt="Scissors"
-      />
-      <p>
-        COMING SOON
-      </p>
->>>>>>> master
   </React.Fragment>
 );
 
