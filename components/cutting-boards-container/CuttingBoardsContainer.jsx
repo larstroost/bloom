dff3edc3da313a7aaf1a8f680c9900cd711a6697
MrNexz <43969336+MrNexz@users.noreply.github.com>
2018-11-30 23:25:27 +0100

    First batch of text. Next batch this weekend. Added note @indexcontainer

--- components/cutting-boards-container/CuttingBoardsContainer.jsx
+++ components/cutting-boards-container/CuttingBoardsContainer.jsx
@@ -11,7 +11,7 @@ class CuttingBoardsContainer extends React.Component {
       <div className="cutting-boards__container">
         <InfoSection
           infoTitle="Snijplanken Vlakken Informatie Pagina"
-          infoText="Informatie over snijplanken vlakken."
+          infoText="Snijplanken vlakken is geen onbelangrijke zaak. Wanneer u uw kostbare snijplank, van welk materiaal dan ook, intensief gebruikt, veroorzaakt u hoe dan ook krassen. In deze krassen kunnen zich ongewenste bacteriën verzamelen. In onze mobiele werkplaats kunt u niet alleen uw dure messen laten slijpen, maar ook uw snijplanken laten bijwerken. Voor een scherpe prijs kunnen wij uw snijplanken vlakken. Dit houdt in dat we de snijplank volledig vlak en krasvrij maken, zodat ze weer aan de Europese HACCP-normen voldoen. Wanneer wij met onze bus bij u in de buurt zijn, kunt u van onze service gebruik maken. Wij rekenen nooit voorrijkosten. In de tijd dat wij uw snijplanken vlakken, ontvangt u van ons leenplanken zodat het werk gewoon door kan gaan. Dat is het voordeel van onze mobiele manier van werken en dus ook de kracht van het bedrijf. Bent u geïnteresseerd in onze service, maar heeft u nog vragen over bepaalde onderwerpen? Of wilt u een afspraak maken met onze professionele slijpers? Neem dan contact met ons op via de contactpagina. Wij helpen u graag verder!"
           src="../../static/photos/chef-knives/kitchen-knife.jpg"
           alt="Snijplanken"
         />
--- components/index-container/IndexContainer.jsx
+++ components/index-container/IndexContainer.jsx
@@ -11,17 +11,23 @@ class IndexContainer extends React.Component {
     return (
       <div className="index__container">
         <InfoSection
-          infoTitle="Wie zijn wij"
-          infoText="Informatie over Bloom."
-          src="../../static/photos/chef-knives/kitchen-knife.jpg"
-          alt="Snijplanken"
+          //infoTitle="Horeca Slijperij Bloom"
+          //infoText="."
+          //src="../../static/photos/chef-knives/kitchen-knife.jpg"
+          //alt="Snijplanken"
+          //deze sectie mag weg
         />
-        <Workflow />
+
+        <Workflow //Alles in een (paar) uur klaar (ipv één dag)
+        />
+
         <InfoSectionReversed
           infoTitle="Onze Geschiedenis"
           infoText="Informatie over de geschiedenis"
           src="../../static/photos/chef-knives/kitchen-knife.jpg"
           alt="Snijplanken"
+          //deze sectie moet onder de vouw
+          //alleen plaatjes (4-5 afbeeldingen op desktop, 3 tablet, 1-2 op mobiel)
         />
       </div>
     );
