4d1341b7df668050e31e94e63544e5eafb9edceb
MrNexz <43969336+MrNexz@users.noreply.github.com>
2019-01-24 00:42:02 +0100

    changed pics in about is

--- components/about-us-container/AboutUsContainer.jsx
+++ components/about-us-container/AboutUsContainer.jsx
@@ -13,14 +13,14 @@ class AboutUsContainer extends React.Component {
         <InfoSection
           infoTitle="Wie zijn wij"
           infoText="Door de kennis en vaardigheid van het uitstekend opgeleide personeel te combineren met de in eigen beheer ontwikkelde slijpapparatuur, heeft Horeca Slijperij Bloom een kwalitatief zeer hoogwaardig slijpproces ontwikkeld. Hierdoor hebben wij een gevarieerd en vast klantenbestand met klanten door heel Nederland en Noord België weten op te bouwen."
-          src="../../static/photos/etc/SlijpenGrayScale2.png"
+          src="../../static/photos/etc/PortretHJ3.png"
           alt="Snijplanken"
         />
         <Divider />
         <InfoSectionReversed
           infoTitle="Onze Geschiedenis"
           infoText="De messenslijperij van Dhr. Bloom is sinds 1985 een begrip in de horeca. Door de exclusieve opleidingen die Dhr. Bloom aan de beroemde Solingen Messerfabrieken in Duitsland heeft gevolgd, heeft hij de juiste kennis in huis om dit vakmanschap in Nederland uit te oefenen. Het bedrijf Bloom Staalwaren was gevestigd. Met twee externe slijpers in dienst, welke door Dhr. Bloom zijn opgeleid, slijpen zij de messen bij de klanten op lokatie. Vanaf 1 januari 2006 voert dit bedrijf verder onder de naam: Horeca Slijperij Bloom B.V. Hoewel Dhr. Bloom geen deel meer uitmaakt van de dagelijkse werkzaamheden, begeleidt hij nog wel de slijpers. Door de kennis en vaardigheid van het uitstekend opgeleide personeel te combineren met de in eigen beheer ontwikkelde slijpapparatuur, heeft Horeca Slijperij Bloom een kwalitatief zeer hoogwaardig slijpproces ontwikkeld. Hierdoor hebben wij een gevarieerd en vast klantenbestand met klanten door heel Nederland en Noord België weten op te bouwen."
-          src="../../static/photos/chef-knives/kitchen-knife.jpg"
+          src="../../static/photos/etc/SlijpenGrayScale-5x4.png"
           alt="Snijplanken"
         />
       </div>
