a4546dfca5bdfca652b6ac11bc67a402cfc4a47f
Lars Troost <42066432+larstroost@users.noreply.github.com>
2019-07-17 01:12:27 +0200

    Removing GTM, next build

--- pages/_document.js
+++ pages/_document.js
@@ -8,16 +8,8 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="/_next/static/style.css" />
-          <script>
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K8D5JQF');
-          </script>
         </Head>
         <body>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8D5JQF" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
           <script src="https://www.google.com/recaptcha/api.js?explicit&hl=nl" async defer></script>
